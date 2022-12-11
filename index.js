419c1afdef8d74edfc0ba7a1c6919fcc99008e0d
Abhinandan Wadhwa <awadhwa_be22@thapar.edu>
2022-12-12 00:42:33 +0530

    upgrade to newer version

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-'use strict';
-
-const axios = require('axios').default;
-
 async function extractSingleColor(imageURI) {
+    const axios = require('axios').default;
     const response = await axios.post('https://image-color-extractor-server-production.up.railway.app/api/getsinglecolor', {
         imageURI: imageURI 
     });
